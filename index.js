8cceaeea5675d6353ece04d5b1cf34375f3593a4
Bryce Kennedy <btken88@gmail.com>
2020-06-09 09:26:27 -0600

    Refactor counting sheep to use ternary

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 // 8kyu - Counting sheep
 function countSheeps(arrayOfSheep) {
-  return arrayOfSheep.reduce(
-    (total, current) => {
-      if (current === true) {
-        return total += 1;
-      }
-      else if (current != true) {
-        return total
-      }
-    }, 0)
+  return arrayOfSheep.reduce((total, current) => {
+    return ((current === true) ? total += 1 : total)
+  }, 0)
 }
