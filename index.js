b593a350535ea939e38389f201392e935f19376e
Bryce Kennedy <btken88@gmail.com>
2020-06-10 08:48:56 -0600

    Solve and refactor descending order problem

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function countSheeps(arrayOfSheep) {
     return ((current === true) ? total += 1 : total)
   }, 0)
 }
+
+// 7kyu - Descending order
+function descendingOrder(n) {
+  let string = n.toString()
+  split = string.split('')
+  sorted = split.sort((a, b) => b - a)
+  joined = sorted.reduce((string, current) => string + current)
+  number = parseInt(joined)
+  return number
+}
+// Descending order refactor
+function descendingOrder(n) {
+  return parseInt(n.toString().split('').sort((a, b) => b - a).reduce((string, current) => string + current))
+}
