04faa2d502de35cb9a809c078d5fb1aa41058645
Bryce Kennedy <btken88@gmail.com>
2020-06-11 08:59:58 -0600

    Solve expanded form problem

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ function countSheeps(arrayOfSheep) {
 // 7kyu - Descending order
 function descendingOrder(n) {
   let string = n.toString()
-  split = string.split('')
-  sorted = split.sort((a, b) => b - a)
-  joined = sorted.reduce((string, current) => string + current)
-  number = parseInt(joined)
+  let split = string.split('')
+  let sorted = split.sort((a, b) => b - a)
+  let joined = sorted.reduce((string, current) => string + current)
+  let number = parseInt(joined)
   return number
 }
 // Descending order refactor
 function descendingOrder(n) {
   return parseInt(n.toString().split('').sort((a, b) => b - a).reduce((string, current) => string + current))
 }
+
+// 6Kyu - Write number in expanded form
+function expandedForm(num) {
+  const numArray = num.toString().split('')
+  let numString = `${numArray[0]}${'0'.repeat(numArray.length - 1)}`
+  for (i = 1; i < numArray.length; i++) {
+    if (numArray[i] != 0) {
+      numString += (` + ${numArray[i]}${'0'.repeat(numArray.length - 1 - i)}`)
+    }
+  }
+  return numString
+}
+expandedForm(70304)
