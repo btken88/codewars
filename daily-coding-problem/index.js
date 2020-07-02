5f2a4467a58ae77aa1e5ea15c7ce25a5768e83a9
Bryce Kennedy <btken88@gmail.com>
2020-07-02 09:14:13 -0600

    Daily Coding Challenge problem 1 solved in JS

---
+++ daily-coding-problem/index.js
@@ -0,0 +1,13 @@
+// Problem 1
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+function addUp(list, k) {
+  for (index in list) {
+    for (secondIndex in list) {
+      if (list[index] + list[secondIndex] === k) { return true }
+    }
+  }
+  return false
+}
+// console.log(addUp([10, 15, 3, 7], 17))
